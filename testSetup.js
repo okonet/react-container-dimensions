8fffc9b885371c72a1a544dab0679828c6df4e42
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-04-27 18:40:44 +0200

    Removed chai from testSetup since it broke everything :(

--- testSetup.js
+++ testSetup.js
@@ -1,19 +1,13 @@
-var jsdom = require('jsdom').jsdom
-var chai = require('chai')
-var chaiEnzyme = require('chai-enzyme')
-chai.use(chaiEnzyme())
+const jsdom = require('jsdom').jsdom
 
-var exposedProperties = ['window', 'navigator', 'document'];
+global.document = jsdom('<!doctype html><html><body></body></html>')
+global.window = document.defaultView
 
-global.document = jsdom('');
-global.window = document.defaultView;
 Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    exposedProperties.push(property);
-    global[property] = document.defaultView[property];
-  }
-});
+    if (typeof global[property] === 'undefined') {
+        global[property] = document.defaultView[property]
+    }
+})
 
-global.navigator = {
-  userAgent: 'node.js'
-};
+global.navigator = { userAgent: 'node.js' }
+global.documentRef = document
