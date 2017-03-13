0beef4c23db7fc8bb3e1b776c7cf67feae9d1e84
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2017-03-13 14:26:37 +0100

    style: Switch to 2 spaces indent + fix some eslint errors

--- src/index.js
+++ src/index.js
@@ -1,59 +1,59 @@
-import React, { Component, PropTypes, Children } from 'react'
+import React, { Children, Component, PropTypes } from 'react'
 import ReactDOM from 'react-dom'
 import elementResizeDetectorMaker from 'element-resize-detector'
 import invariant from 'invariant'
 
 export default class ContainerDimensions extends Component {
 
-    static propTypes = {
-        children: PropTypes.oneOfType([PropTypes.element, PropTypes.func]).isRequired
-    }
+  static propTypes = {
+    children: PropTypes.oneOfType([PropTypes.element, PropTypes.func]).isRequired
+  }
 
-    static getDomNodeDimensions(node) {
-        const { top, right, bottom, left, width, height } = node.getBoundingClientRect()
-        return { top, right, bottom, left, width, height }
-    }
+  static getDomNodeDimensions(node) {
+    const { top, right, bottom, left, width, height } = node.getBoundingClientRect()
+    return { top, right, bottom, left, width, height }
+  }
 
-    constructor() {
-        super()
-        this.state = {
-            initiated: false
-        }
-        this.onResize = this.onResize.bind(this)
+  constructor() {
+    super()
+    this.state = {
+      initiated: false
     }
+    this.onResize = this.onResize.bind(this)
+  }
 
-    componentDidMount() {
-        this.parentNode = ReactDOM.findDOMNode(this).parentNode
-        this.elementResizeDetector = elementResizeDetectorMaker({
-            strategy: 'scroll',
-            callOnAdd: false
-        })
-        this.elementResizeDetector.listenTo(this.parentNode, this.onResize)
-        this.onResize()
+  componentDidMount() {
+    this.parentNode = ReactDOM.findDOMNode(this).parentNode
+    this.elementResizeDetector = elementResizeDetectorMaker({
+      strategy: 'scroll',
+      callOnAdd: false
+    })
+    this.elementResizeDetector.listenTo(this.parentNode, this.onResize)
+    this.onResize()
+  }
+
+  componentWillUnmount() {
+    this.elementResizeDetector.uninstall(this.parentNode)
+  }
+
+  onResize() {
+    const clientRect = ContainerDimensions.getDomNodeDimensions(this.parentNode)
+    this.setState({
+      initiated: true,
+      ...clientRect
+    })
+  }
+
+  render() {
+    invariant(this.props.children, 'Expected children to be one of function or React.Element')
+
+    if (!this.state.initiated) {
+      return <div />
     }
-
-    componentWillUnmount() {
-        this.elementResizeDetector.uninstall(this.parentNode)
+    if (typeof this.props.children === 'function') {
+      const renderedChildren = this.props.children(this.state)
+      return renderedChildren && Children.only(renderedChildren)
     }
-
-    onResize() {
-        const clientRect = ContainerDimensions.getDomNodeDimensions(this.parentNode)
-        this.setState({
-            initiated: true,
-            ...clientRect
-        })
-    }
-
-    render() {
-        invariant(this.props.children, 'Expected children to be one of function or React.Element')
-
-        if (!this.state.initiated) {
-            return <div />
-        }
-        if (typeof this.props.children === 'function') {
-            const renderedChildren = this.props.children(this.state)
-            return renderedChildren && Children.only(renderedChildren)
-        }
-        return Children.only(React.cloneElement(this.props.children, this.state))
-    }
+    return Children.only(React.cloneElement(this.props.children, this.state))
+  }
 }
--- tests/index.js
+++ tests/index.js
@@ -10,188 +10,188 @@ const MyComponent = ({ width, height }) => <span>{width}, {height}</span> // esl
 
 describe('react-container-dimensions', () => {
 
-    it('should throw without children', () => {
-        expect(() => mount(<div><ContainerDimensions /></div>))
-            .to.throw('Expected children to be one of function or React.Element')
+  it('should throw without children', () => {
+    expect(() => mount(<div><ContainerDimensions /></div>))
+      .to.throw('Expected children to be one of function or React.Element')
+  })
+
+  it('calls componentDidMount', (done) => {
+    spy(ContainerDimensions.prototype, 'componentDidMount')
+    spy(ContainerDimensions.prototype, 'componentWillUnmount')
+    const wrapper = mount(
+      <div>
+        <ContainerDimensions>
+          <MyComponent />
+        </ContainerDimensions>
+      </div>
+      , { attachTo: document.getElementById('root') })
+    expect(wrapper.find('div').length).to.eq(1)
+    expect(ContainerDimensions.prototype.componentDidMount.calledOnce).to.be.true
+    ContainerDimensions.prototype.componentDidMount.restore()
+    setTimeout(() => {
+      wrapper.unmount()
+      expect(ContainerDimensions.prototype.componentWillUnmount.calledOnce).to.be.true
+      ContainerDimensions.prototype.componentWillUnmount.restore()
+      done()
+    }, 0)
+  })
+
+  it('calls onResize on mount', () => {
+    spy(ContainerDimensions.prototype, 'onResize')
+    mount(
+      <div>
+        <ContainerDimensions>
+          <MyComponent />
+        </ContainerDimensions>
+      </div>
+    )
+    expect(ContainerDimensions.prototype.onResize.calledOnce).to.be.true
+    ContainerDimensions.prototype.onResize.restore()
+  })
+
+  xit('calls onResize when parent has been resized', (done) => {
+    spy(ContainerDimensions.prototype, 'onResize')
+    const wrapper = mount(
+      <div ref="node" id="node" style={{ width: 10 }}>
+        <ContainerDimensions>
+          <MyComponent />
+        </ContainerDimensions>
+      </div>
+      , { attachTo: document.getElementById('root') })
+    const el = wrapper.render()
+    el.css('width', 10)
+    setTimeout(() => {
+      el.css('width', 100) // Triggering onResize event
+      expect(ContainerDimensions.prototype.onResize.calledTwice).to.be.true
+      ContainerDimensions.prototype.onResize.restore()
+      done()
+    }, 10)
+  })
+
+  it('onResize sets state with all keys and values from getBoundingClientRect', () => {
+    const styles = { top: 100, width: 200 }
+    stub(ContainerDimensions, 'getDomNodeDimensions', () => ({
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      width: 0,
+      height: 0,
+      ...styles
+    }))
+    const wrapper = mount(
+      <div style={styles}>
+        <ContainerDimensions>
+          <MyComponent />
+        </ContainerDimensions>
+      </div>
+      , { attachTo: document.getElementById('root') })
+
+    wrapper.render()
+    expect(wrapper.find(MyComponent).props()).to.have.keys([
+      'initiated',
+      'top',
+      'right',
+      'bottom',
+      'left',
+      'width',
+      'height'])
+    expect(wrapper.find(MyComponent)).to.have.prop('top', 100)
+    expect(wrapper.find(MyComponent)).to.have.prop('width', 200)
+    ContainerDimensions.getDomNodeDimensions.restore()
+  })
+
+  it('should initially render an empty placeholder', () => {
+    const wrapper = mount(
+      <ContainerDimensions>
+        <MyComponent />
+      </ContainerDimensions>
+    )
+    wrapper.setState({
+      initiated: false
     })
 
-    it('calls componentDidMount', (done) => {
-        spy(ContainerDimensions.prototype, 'componentDidMount')
-        spy(ContainerDimensions.prototype, 'componentWillUnmount')
-        const wrapper = mount(
-            <div>
-                <ContainerDimensions>
-                    <MyComponent />
-                </ContainerDimensions>
-            </div>
-        , { attachTo: document.getElementById('root') })
-        expect(wrapper.find('div').length).to.eq(1)
-        expect(ContainerDimensions.prototype.componentDidMount.calledOnce).to.be.true
-        ContainerDimensions.prototype.componentDidMount.restore()
-        setTimeout(() => {
-            wrapper.unmount()
-            expect(ContainerDimensions.prototype.componentWillUnmount.calledOnce).to.be.true
-            ContainerDimensions.prototype.componentWillUnmount.restore()
-            done()
-        }, 0)
+    expect(wrapper.find(ContainerDimensions)).to.have.exactly(1).descendants('div')
+    expect(wrapper).to.have.html('<div data-reactroot=""></div>')
+    expect(wrapper.find(MyComponent).length).to.eq(0)
+
+    wrapper.setState({
+      initiated: true
     })
 
-    it('calls onResize on mount', () => {
-        spy(ContainerDimensions.prototype, 'onResize')
-        mount(
-            <div>
-                <ContainerDimensions>
-                    <MyComponent />
-                </ContainerDimensions>
-            </div>
-        )
-        expect(ContainerDimensions.prototype.onResize.calledOnce).to.be.true
-        ContainerDimensions.prototype.onResize.restore()
+    expect(wrapper.find(MyComponent).length).to.eq(1)
+  })
+
+  it('should pass dimensions as props to its children', () => {
+    const wrapper = mount(
+      <ContainerDimensions>
+        <MyComponent />
+      </ContainerDimensions>
+    )
+    wrapper.setState({
+      top: 0,
+      right: 100,
+      bottom: 300,
+      left: 200,
+      width: 100,
+      height: 200
     })
+    expect(wrapper.find(MyComponent)).to.have.length(1)
+    expect(wrapper.find(MyComponent)).to.have.prop('top', 0)
+    expect(wrapper.find(MyComponent)).to.have.prop('right', 100)
+    expect(wrapper.find(MyComponent)).to.have.prop('bottom', 300)
+    expect(wrapper.find(MyComponent)).to.have.prop('left', 200)
+    expect(wrapper.find(MyComponent)).to.have.prop('width', 100)
+    expect(wrapper.find(MyComponent)).to.have.prop('height', 200)
+  })
 
-    xit('calls onResize when parent has been resized', (done) => {
-        spy(ContainerDimensions.prototype, 'onResize')
-        const wrapper = mount(
-            <div ref="node" id="node" style={{ width: 10 }}>
-                <ContainerDimensions>
-                    <MyComponent />
-                </ContainerDimensions>
-            </div>
-        , { attachTo: document.getElementById('root') })
-        const el = wrapper.render()
-        el.css('width', 10)
-        setTimeout(() => {
-            el.css('width', 100) // Triggering onResize event
-            expect(ContainerDimensions.prototype.onResize.calledTwice).to.be.true
-            ContainerDimensions.prototype.onResize.restore()
-            done()
-        }, 10)
+  it('should pass dimensions as function arguments', () => {
+    const wrapper = mount(
+      <ContainerDimensions>
+        {
+          ({ left, width, height }) => // eslint-disable-line
+            <MyComponent
+              left={left + 10}
+              width={width + 10}
+              height={height + 10}
+            />
+        }
+      </ContainerDimensions>
+    )
+    wrapper.setState({
+      left: 20,
+      width: 100,
+      height: 200
     })
+    expect(wrapper.find(MyComponent)).to.have.length(1)
+    expect(wrapper.find(MyComponent)).to.have.prop('left', 30)
+    expect(wrapper.find(MyComponent)).to.have.prop('width', 110)
+    expect(wrapper.find(MyComponent)).to.have.prop('height', 210)
+  })
 
-    it('onResize sets state with all keys and values from getBoundingClientRect', () => {
-        const styles = { top: 100, width: 200 }
-        stub(ContainerDimensions, 'getDomNodeDimensions', () => ({
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-            width: 0,
-            height: 0,
-            ...styles
-        }))
-        const wrapper = mount(
-            <div style={styles}>
-                <ContainerDimensions>
-                    <MyComponent />
-                </ContainerDimensions>
-            </div>
-        , { attachTo: document.getElementById('root') })
-
-        wrapper.render()
-        expect(wrapper.find(MyComponent).props()).to.have.keys([
-            'initiated',
-            'top',
-            'right',
-            'bottom',
-            'left',
-            'width',
-            'height'])
-        expect(wrapper.find(MyComponent)).to.have.prop('top', 100)
-        expect(wrapper.find(MyComponent)).to.have.prop('width', 200)
-        ContainerDimensions.getDomNodeDimensions.restore()
+  it('should work with SVG elements', () => {
+    const wrapper = mount(
+      <ContainerDimensions>
+        <rect />
+      </ContainerDimensions>
+    )
+    wrapper.setState({
+      width: 100,
+      height: 200
     })
+    expect(wrapper.find('rect')).to.have.length(1)
+    expect(wrapper.find('rect')).to.have.prop('width', 100)
+    expect(wrapper.find('rect')).to.have.prop('height', 200)
+  })
 
-    it('should initially render an empty placeholder', () => {
-        const wrapper = mount(
-            <ContainerDimensions>
-                <MyComponent />
-            </ContainerDimensions>
-        )
-        wrapper.setState({
-            initiated: false
-        })
-
-        expect(wrapper.find(ContainerDimensions)).to.have.exactly(1).descendants('div')
-        expect(wrapper).to.have.html('<div></div>')
-        expect(wrapper.find(MyComponent).length).to.eq(0)
-
-        wrapper.setState({
-            initiated: true
-        })
-
-        expect(wrapper.find(MyComponent).length).to.eq(1)
-    })
-
-    it('should pass dimensions as props to its children', () => {
-        const wrapper = mount(
-            <ContainerDimensions>
-                <MyComponent />
-            </ContainerDimensions>
-        )
-        wrapper.setState({
-            top: 0,
-            right: 100,
-            bottom: 300,
-            left: 200,
-            width: 100,
-            height: 200
-        })
-        expect(wrapper.find(MyComponent)).to.have.length(1)
-        expect(wrapper.find(MyComponent)).to.have.prop('top', 0)
-        expect(wrapper.find(MyComponent)).to.have.prop('right', 100)
-        expect(wrapper.find(MyComponent)).to.have.prop('bottom', 300)
-        expect(wrapper.find(MyComponent)).to.have.prop('left', 200)
-        expect(wrapper.find(MyComponent)).to.have.prop('width', 100)
-        expect(wrapper.find(MyComponent)).to.have.prop('height', 200)
-    })
-
-    it('should pass dimensions as function arguments', () => {
-        const wrapper = mount(
-            <ContainerDimensions>
-                {
-                    ({ left, width, height }) => // eslint-disable-line
-                        <MyComponent
-                            left={left + 10}
-                            width={width + 10}
-                            height={height + 10}
-                        />
-                }
-            </ContainerDimensions>
-        )
-        wrapper.setState({
-            left: 20,
-            width: 100,
-            height: 200
-        })
-        expect(wrapper.find(MyComponent)).to.have.length(1)
-        expect(wrapper.find(MyComponent)).to.have.prop('left', 30)
-        expect(wrapper.find(MyComponent)).to.have.prop('width', 110)
-        expect(wrapper.find(MyComponent)).to.have.prop('height', 210)
-    })
-
-    it('should work with SVG elements', () => {
-        const wrapper = mount(
-            <ContainerDimensions>
-                <rect />
-            </ContainerDimensions>
-        )
-        wrapper.setState({
-            width: 100,
-            height: 200
-        })
-        expect(wrapper.find('rect')).to.have.length(1)
-        expect(wrapper.find('rect')).to.have.prop('width', 100)
-        expect(wrapper.find('rect')).to.have.prop('height', 200)
-    })
-
-    it('should not create a DOM element for itself', () => {
-        const wrapper = mount(
-            <h1>
-                <ContainerDimensions>
-                    <span>Test</span>
-                </ContainerDimensions>
-            </h1>
-        )
-        expect(wrapper.html()).to.contain('<h1><span width="0" height="0">Test</span>')
-    })
+  it('should not create a DOM element for itself', () => {
+    const wrapper = mount(
+      <h1>
+        <ContainerDimensions>
+          <span>Test</span>
+        </ContainerDimensions>
+      </h1>
+    )
+    expect(wrapper.html()).to.contain('<h1><span width="0" height="0">Test</span>')
+  })
 })
